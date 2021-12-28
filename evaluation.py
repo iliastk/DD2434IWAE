954d3cc0a6cacfd8bdb11b6f65078ec332054b4b
Martin Iglesias <100384068@alumnos.uc3m.es>
2021-12-28 12:35:30 +0100

    changed model file  naming to epoch-loss-logpx

--- evaluation.py
+++ evaluation.py
@@ -46,8 +46,7 @@ def log_results(best_model_dir, test_results, train_results, curr_epoch, num_epo
     loss, log_px = test_results['loss'], test_results['log_px']
     out_result = out_result + \
         f'\t == \t Test [loss: {loss.item():.3f}, NLL:{log_px.item():.3f}]'
-    best_model_filename = f'{best_model_dir}/Loss:{loss:.3f}-LogPx:{log_px:.3f}-Epoch:{curr_epoch}.pt'
-
+    best_model_filename = f'{best_model_dir}/Epoch:{curr_epoch}-Loss:{loss:.2f}-LogPx:{log_px:.2f}.pt'
     # TODO: Log to tensorboard
 
     print(out_result)
