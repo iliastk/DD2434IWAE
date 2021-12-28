25f894c80e7577b962c618a40445e969edda7135
Martin Iglesias Goyanes <miglesias@arvak.tsc.uc3m.es>
2021-12-28 18:30:39 +0100

    Revert "started refactor for easy experimentatino"
    This reverts commit 54d81a9c742e76bc581d01cef4edaac99f2b66df.

--- main.py
+++ main.py
@@ -1,20 +1,73 @@
 from datasets import BinarizedMNIST
 from vae import VAE
 
+from datetime import datetime
+from pathlib import Path
+
+import torchvision
+from torchvision import transforms
+
 import torch
 import random
 import numpy as np
 
-from experiments.base import experiment as base_experiment
-from experiment import launch_experiment
+from evaluation import train_epoch, test_epoch, log_results 
 
 def main():
-   # TODO: Log to TensorBoard
-   torch.manual_seed(123)
-   random.seed(123)
-   np.random.seed(123)
+    # TODO: Proper weight init
+    # TODO: Proper bias init
+    # TODO: Log to TensorBoard
+    torch.manual_seed(123)
+    random.seed(123)
+    np.random.seed(123)
 
-   launch_experiment(base_experiment)
+    batch_size = 20
+    data = {
+        "train": BinarizedMNIST(train=True, root_path="./data/"),
+        "val": None,
+        "test": BinarizedMNIST(train=False, root_path="./data/")
+    }
+    data_loader = {
+        "train": torch.utils.data.DataLoader(dataset=data["train"], batch_size=batch_size, shuffle=True, num_workers=8),
+        "val": None,
+        "test": torch.utils.data.DataLoader(dataset=data["test"], batch_size=batch_size, shuffle=True, num_workers=8)
+    }
 
-if __name__ == "__main__":
-    main()
+    X_dim = 784  # 28x28
+    Z_dim = 50
+    H_dim = {"encoder": [200, 200], "decoder": [200, 200]}
+    num_samples = 1
+    model_bias = data["train"].get_bias()
+    model = VAE(X_dim, H_dim, Z_dim, num_samples,
+                encoder='Gaussian', decoder='Bernoulli', bias=model_bias)
+    print(model)
+
+    lr = 0.001  # TODO: Make lr scheduable as in Burda et al.
+    beta_1, beta_2, epsilon = 0.9, 0.999, 1e-4
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=lr, betas=(beta_1, beta_2), eps=epsilon)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    milestones = np.cumsum([3 ** i for i in range(8)])
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(
+        optimizer, milestones=milestones, gamma=10 ** (-1 / 7), verbose=True
+    )
+
+    experiment_name = 'base'
+    now = datetime.now()
+    timestamp = now.strftime("%d-%m-%Y-%H:%M:%S")
+    best_model_dir = f'results/{experiment_name}/{timestamp}'
+    Path(best_model_dir).mkdir(parents=True, exist_ok=True)
+
+
+    num_epochs = 3280  # TODO: Set epochs like Burda et al.
+    for epoch in range(num_epochs):
+        train_results = train_epoch(optimizer, scheduler, batch_size, data_loader["train"], model, X_dim)
+        test_results = test_epoch(data_loader["test"], batch_size, model, X_dim)
+        log_results(best_model_dir, test_results, train_results, epoch, num_epochs, model)
+
+
+    # TODO: Check if loss is nan
+    # TODO: Check if improvement in test loss is too small
+
+main()
--- vae.py
+++ vae.py
@@ -8,11 +8,10 @@ twoPI = torch.tensor(2*np.pi)
 
 
 class VAE(nn.Module):
-    def __init__(self, X_dim, H_dim, Z_dim, num_samples, encoder='Gaussian', decoder='Bernoulli', bias=None, loss_threshold=0.01):
+    def __init__(self, X_dim, H_dim, Z_dim, num_samples, encoder='Gaussian', decoder='Bernoulli', bias=None):
         super(VAE, self).__init__()
         self.num_samples = num_samples
-        self.best_test_loss = np.inf
-        self.loss_threshold = loss_threshold
+        self.best_test_NLL = -np.inf
         # encoder network - q(z|x)
         if encoder == 'Gaussian':
             self.encoder = GaussianSampler(X_dim, H_dim["encoder"], Z_dim)
@@ -107,7 +106,7 @@ class VAE(nn.Module):
     def set_gpu_use(self):
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
-        print(f'Using device [{self.device}].')
+        print(f'Using device [{self.device}] to train network.')
         self.to(self.device)
 
 
