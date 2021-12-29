96d78cc585ca05b4df13126b54eb394f117158e4
Martin Iglesias Goyanes <miglesias@arvak.tsc.uc3m.es>
2021-12-29 23:49:12 +0100

    added todo's

--- main.py
+++ main.py
@@ -8,13 +8,19 @@ import numpy as np
 from experiments.base import experiment as base_experiment
 from experiment import launch_experiment
 
-def main():
-   # TODO: Log to TensorBoard
-   torch.manual_seed(123)
-   random.seed(123)
-   np.random.seed(123)
 
-   launch_experiment(base_experiment)
+def main():
+    # TODO: Fancy Logs to TensorBoard
+    # TODO: Instead of hardcoded pdf use samplers.pdf
+    # TODO: Gradient clipping
+    # TODO: Detect hidden units
+    # TODO: Bernoulli Binarization?
+    torch.manual_seed(123)
+    random.seed(123)
+    np.random.seed(123)
+
+    launch_experiment(base_experiment)
+
 
 if __name__ == "__main__":
     main()
